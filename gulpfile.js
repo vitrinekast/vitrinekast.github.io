2de783f5f3e2f817d442418b2980e6b0f664d2b7
rschuurmans <rosa@wetteloos.nl>
2020-12-31 15:06:00 +0100

    clean gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,25 +5,53 @@ const path = require("path");
 const fs = require('fs');
 const browserSync = require('browser-sync').create();
 
-const doSomething = () => {
-
-}
-
-function watchFiles() {
-  browserSync.init({
+const config = {
+  html: {
+    src: ['**/*.html', '!node_modules/**/*.html', '!boilerplate/*.html'],
+    dest: './',
+    title: /{{PAGE_TITLE}}/
+  },
+  js: {
+    src: ['*/*.js'],
+    dest: './',
+    title: /{{PAGE_TITLE}}/
+  },
+  exports: {
+    dest: '_exports'
+  },
+  puppeteer: {
+    headless: true,
+    args: [
+      '--disable-web-security'
+    ]
+  },
+  browsersync: {
+    port: 3000,
     server: {
       baseDir: './'
     },
 
     notify: false
-  });
+  }
+}
 
-  gulp.watch('*/*.js', doSomething).on("change", function (file) {
-    console.log(file);
-    gulp
-      .src(file)
+const setTitle = () => {
+  return gulp.src(config.html.src)
+    .pipe(tap((file) => {
+      file.contents = new Buffer.from(String(file.contents)
+        .replace(config.html.title, path.basename(file.dirname))
+      );
+    }))
+    .pipe(gulp.dest(config.html.dest));
+}
+
+function watchFiles() {
+  browserSync.init(config.browsersync);
+
+  gulp.watch(config.js.src).on("change", function (file) {
+    gulp.src(file)
       .pipe(tap(async (file) => {
-        var dir = "./" + path.basename(file.dirname) + "/_exports";
+        var dir = `./${path.basename(file.dirname)}/${config.exports.dest}`;
         if (!fs.existsSync(dir)) {
           fs.mkdirSync(dir);
           console.log('📁  folder created:', dir);
@@ -31,21 +59,14 @@ function watchFiles() {
       }))
 
       .pipe(tap(async (file) => {
-        const browser = await puppeteer.launch({
-          headless: true, args: [
-            '--disable-web-security',
-          ]
-        });
-
+        const browser = await puppeteer.launch(config.puppeteer);
         const page = await browser.newPage();
-        // await page.setViewport({ width: 1200, height: 6000 });
-        await page.goto("http://localhost:3000/" + path.basename(file.dirname), { timeout: 3000 });
-        await page.evaluate(() => {
 
+        await page.goto(`http://localhost:${config.browsersync.port}/${path.basename(file.dirname)}`, { timeout: 3000 });
+        await page.evaluate(() => {
           document.querySelectorAll('button').forEach(element => {
             element.style.visibility = "hidden";
           })
-
         })
 
         await page.screenshot({
@@ -53,15 +74,11 @@ function watchFiles() {
           fullPage: true,
           omitBackground: true
         });
+
         await browser.close();
       }))
   });
 }
 
-
-
-const watch = gulp.series(watchFiles);
-// const build = gulp.series(screens);
-
-exports.watch = watch;
-// exports.default = build;
+exports.watch = gulp.series(setTitle, watchFiles);
+exports.clean = gulp.series(setTitle);
